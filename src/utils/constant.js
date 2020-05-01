787c52679988209120dc12ff3cffd2c34f3a4ebd
Hina Gupta <hinagupta25sept@gmail.com>
2020-05-01 16:05:45 +0530

    Updating readme file

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,4 @@
-export const DOMAIN = "http://localhost:3000";
+export const DOMAIN =
+  "http://contestarena27.s3-website.ap-south-1.amazonaws.com";
 export const REDIRECT_URI =
-  "http://localhost:3000/callback";
+  "http://contestarena27.s3-website.ap-south-1.amazonaws.com/callback";
